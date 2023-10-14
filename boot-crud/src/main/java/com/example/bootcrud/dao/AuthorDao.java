e4bfaa9da9f08afa5f687cfb3041f7cb174eda91
ChitHninWai <chithninwaiucst@gmail.com>
2023-10-14 16:36:06 +0630

    delete

--- boot-crud/src/main/java/com/example/bootcrud/dao/AuthorDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.bootcrud.dao;
-
-import com.example.bootcrud.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorDao extends JpaRepository<Author,Integer> {
-}
--- boot-crud/src/main/java/com/example/bootcrud/dao/BookDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.bootcrud.dao;
-
-import com.example.bootcrud.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookDao extends JpaRepository<Book,Integer> {
-}
